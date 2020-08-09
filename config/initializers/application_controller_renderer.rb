3ea1631fc6667e741d62b7f5b8867c360bee86f2
有澤孝輝 <teruuuuun72@gmail.com>
2020-08-09 20:01:43 +0900

    [Add]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8bc96e7f47f8a2c67cfcdee1ee97413af59fddc86b8da2c8426ab840242b452466cd26a9bbe40822ae84bfbf618798fac239ab5a529c0569071aec013892ed9d'
+
